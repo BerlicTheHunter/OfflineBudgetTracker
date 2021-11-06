5c99c3ebaa9bd30dfaa63df1120d8b0208416156
Berlic <bsorrell3@gmail.com>
2021-11-05 20:29:24 -0400

    Working app, online and offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,63 @@
-const files_to_casche =[
-"/",
+// * ----- Variable Set up ----- * \\
+const files_to_cache =[
+// "/",
 "/index.html",
 "/index.js",
-"/manafest.webmanifest",
+"/manifest.webmanifest",
 "/db.js",
+"/styles.css",
 "/icons/icon-192x192.png",
-"/icons/icon-512x512.png"
-]
+"/icons/icon-512x512.png",
+"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+"https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+];
 
-self.addEventListener("install", function(event) {
-  console.log( "Service Worker Installed");
+// * ----- Install Service Worker ----- * \\
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open('static')
+      .then( cache => {
+       return cache.addAll(files_to_cache);
+    })
+  );
+  console.log( "Service Worker Installed");  
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+// * ----- Activate Service Worker ----- * \\
+self.addEventListener("activate", (event) => {
   console.log("Service Worker Activated");
 });
 
-self.addEventListener("fetch", () => {
-  console.log("Service Worker Heard A Fetch");
-});
+
+//  * ----- Fetch Listener ----- * \\
+self.addEventListener("fetch", (event) => {
+  // console.log("Service Worker Heard A Fetch");
+  let status= navigator.onLine;
+  console.log("Browser Online: " + status);
+  
+//   event.respondWIth(
+//       caches.match(event.request).then((cacheRes) => {return cacheRes || fetch(event.request);})
+//   )
+// });
+
+
+if (event.request.url.startsWith(self.location.origin) && !status) {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return caches.open(RUNTIME).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+}
+})
